1fe399e134986b7cb3cc77213c4d1ee702718275
bemnet-21 <anjejobemnet@gmail.com>
2025-06-21 19:57:41 +0300

    added state

--- alx-project-0x08/interfaces/index.ts
+++ alx-project-0x08/interfaces/index.ts
@@ -3,3 +3,12 @@ import { ReactNode } from "react";
 export interface ReactComponentProps {
     children: ReactNode
 }
+export interface GeneratedImageProps {
+    imageUrl: string
+    prompt: string
+    width?: string
+    height?: string
+    action: (imagePath: string) => void
+}
+
+export type ImageProps = Pick<GeneratedImageProps, "imageUrl" | "prompt">
--- alx-project-0x08/pages/index.tsx
+++ alx-project-0x08/pages/index.tsx
@@ -1,8 +1,20 @@
-import React, { useState } from "react";
+import ImageCard from "@/components/common/ImageCard";
+import { ImageProps } from "@/interfaces";
+import { useState } from "react";
+
+
 
 const Home: React.FC = () => {
+  const [prompt, setPrompt] = useState<string>("");
+  const [imageUrl, setImageUrl] = useState<string>("");
+  const [generatedImages, setGeneratedImages] = useState<ImageProps[]>(
+    []
+  );
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+
   const handleGenerateImage = async () => {
-    console.log("Generating Images")
+    console.log("Generating Images");
   };
 
   return (
@@ -10,15 +22,32 @@ const Home: React.FC = () => {
       <div className="flex flex-col items-center">
         <h1 className="text-4xl font-bold mb-2">Image Generation App</h1>
         <p className="text-lg text-gray-700 mb-4">
-          Generate stunning images based on your prompts
+          Generate stunning images based on your prompts!
         </p>
+
         <div className="w-full max-w-md">
-          <input type="text" placeholder="Enter your prompt here" className="w-full p-3 border border-gray-300 rounded-lg mb-4" />
-          <button onClick={handleGenerateImage} className="w-full p-3 bg-blue-600 text-white rounded-lg hover:bg-bluue-700 transition duration-200">Generate Image</button>
+          <input
+            type="text"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            placeholder="Enter your prompt here..."
+            className="w-full p-3 border border-gray-300 rounded-lg mb-4"
+          />
+          <button
+            onClick={handleGenerateImage}
+            className="w-full p-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-200"
+          >
+            {/* {
+              isLoading ? "Loading..." : "Generate Image"
+            } */}
+            Generate Image
+          </button>
         </div>
+
+        {imageUrl && <ImageCard action={() => setImageUrl(imageUrl)} imageUrl={imageUrl} prompt={prompt} />}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
